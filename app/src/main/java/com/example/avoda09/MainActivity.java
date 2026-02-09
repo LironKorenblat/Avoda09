0febdc0749895244dd1b6b656d060f806c368386
liron_korenblat <lironkorenblat14@gmai.com>
2026-02-09 09:41:57 +0200

    second commit

--- app/src/main/java/com/example/avoda09/MainActivity.java
+++ app/src/main/java/com/example/avoda09/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.avoda09;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    EditText etn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        etn = findViewById(R.id.etn);
+    }
+
+    public void clicked(View view) {
+        String st = etn.getText().toString();
+        int num = Integer.parseInt(st);
+
     }
 }
